13e848b3b6250f842e3c97e9e0e918f4f04640ac
ctncodes <35818754+ctncodes@users.noreply.github.com>
2019-11-22 18:33:46 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,15 @@
 let northX;
 let westY;
 // var mic;
+let eagleEye1;
+let eagleEye2;
+let eagleEye3;
+let eagleEye4;
+let eagleEye5;
+let eagleEye6;
+let eagleEye7;
+let eagleEye8;
+let eagleEye9;
 let musicNote1;
 let musicNote2;
 let musicNote3;
@@ -15,12 +24,20 @@ function setup() {
   art3 = loadImage('assets/Landscape Near Murnau, by Alexej von Jawlensky.png');
   art4 = loadImage('assets/Varoom!, by Roy Lichtenstein.png');
   createCanvas(600,600);
-  musicNote1 = new BeamedNote(width/2,height*3/8);
-  musicNote2 = new WholeNote(width/8,height/8);
-  musicNote3 = new HalfNote(width/16,height*5/8);
-  musicNote4 = new QuarterNote(width*3/8,height*7/8);
-  // console.log(musicNote3);
+  // musicNote1 = new BeamedNote(300,225);
+  // musicNote2 = new WholeNote(75,75);
+  // musicNote3 = new HalfNote(37.5,375);
+  // musicNote4 = new QuarterNote(225,525);
   // mic = new p5.AudioIn()
+  eagleEye1 = new TailFeatherPattern(width*5/64,height*13/64);
+  eagleEye2 = new TailFeatherPattern(width*5/64,height*23/64);
+  eagleEye3 = new TailFeatherPattern(width*7/32,height/8);
+  eagleEye4 = new TailFeatherPattern(width*7/32,height*9/32);
+  eagleEye5 = new TailFeatherPattern(width*7/32,height*7/16);
+  eagleEye6 = new TailFeatherPattern(width*23/64,height*13/64);
+  eagleEye7 = new TailFeatherPattern(width*23/64,height*23/64);
+  eagleEye8 = new TailFeatherPattern(width/2,height*9/32);
+  eagleEye9 = new TailFeatherPattern(width/2,height*7/16);
   // mic.start();
 }
 function draw() {
@@ -38,47 +55,48 @@ function draw() {
     //Waddle
     fill(227,38,54);
     beginShape();
-    curveVertex(width*.78,height*.33);
-    curveVertex(width*.78,height*.33);
-    curveVertex(width*.78,height*.4);
-    curveVertex(width*.82,height*.39);
-    curveVertex(width*.78,height*.33);
-    curveVertex(width*.78,height*.33);
+    curveVertex(468,198);
+    curveVertex(468,198);
+    curveVertex(468,240);
+    curveVertex(492,234);
+    curveVertex(468,198);
+    curveVertex(468,198);
     endShape();
     //Beak
     fill(218,165,32);
-  triangle(width*.78,height/5,width*.78,height/4,width*.88,height/4);   triangle(width*.78,height*.35,width*.78,height*.3,width*.88,height*.3);
-    //Open Mouth
+    triangle(468,120,468,150,528,150);
+    triangle(468,210,468,180,528,180);
+    //Open Beak
     fill(0);
-    rect(width*.78,height/4,width/80,height/20);
+    rect(468,150,8,30);
   } else {
     //Waddle
     fill(227,38,54);
     beginShape();
-    curveVertex(width*.78,height*.28);
-    curveVertex(width*.78,height*.28);
-    curveVertex(width*.78,height*.35);
-    curveVertex(width*.82,height*.34);
-    curveVertex(width*.78,height*.28);
-    curveVertex(width*.78,height*.28);
+    curveVertex(468,168);
+    curveVertex(468,168);
+    curveVertex(468,210);
+    curveVertex(492,204);
+    curveVertex(468,168);
+    curveVertex(468,168);
     endShape();
     //Beak
     fill(218,165,32);
-  triangle(width*.78,height/5,width*.78,height*.3,width*.88,height/4);
-  musicNote1.display();
-  musicNote1.move();
-  musicNote2.display();
-  musicNote2.move();
-  musicNote3.display();
-  musicNote3.move();
-  musicNote4.display();
-  musicNote4.move();
+  triangle(468,120,468,180,528,150);
+  // musicNote1.display();
+  // musicNote1.move();
+  // musicNote2.display();
+  // musicNote2.move();
+  // musicNote3.display();
+  // musicNote3.move();
+  // musicNote4.display();
+  // musicNote4.move();
   }
 }
 function drawTheMorning(xPos,yPos) {
   // if (constrain(height-micLevel*height*5,0,height)<height/3) {
   //   fill(255,204,51);
-  //   background(135,206,235);  
+  //   background(135,206,235);
   // } else if (constrain(height-micLevel*height*5,0, height)>height*2/3) {
   //   fill(255,90,37);
   //   background(25,25,112);
@@ -86,7 +104,7 @@ function drawTheMorning(xPos,yPos) {
   //   fill(255,147,44);
   //   background(80,116,174);
   // }
-  // circle(width*13/16,constrain(height-micLevel*height*5,0, height),width*3/8);
+  // circle(487.5,constrain(height-micLevel*height*5,0, height),225);
   if (mouseY<height/3) {
     fill(255,204,51);
     // background(135,206,235);
@@ -100,147 +118,102 @@ function drawTheMorning(xPos,yPos) {
     // background(80,116,174);
     background(art2);
   }
-  circle(xPos,yPos,width*3/16 );
+  circle(xPos,yPos,112.5);
 }
 function drawBeautifulBody() {
   //Tailfeathers
   fill(82,40,168);
   noStroke();
-  arc(width*7/32,height/2,width*7/8,height*7/8,PI,2*PI);
+  arc(131.25,300,525,525,PI,2*PI);
   // ellipseMode(RADIUS);
   // fill(255);
-  // ellipse(width*7/32,height/8,width/16,height/16);
+  // ellipse(131.25,75,37.5,37.5);
   // ellipseMode(CENTER);
   // fill(100);
-  // ellipse(width*7/32,height/8,width/16,height/16);
+  // ellipse(131.25,75,37.5,37.5);
   // //Left Wing
   // fill(0,71,171);
   // noStroke();
-  // arc(width*7/8,height/2,width*3/8,height*3/8,0,PI);
+  // arc(525,300,225,225,0,PI);
   //Body
   fill(111,78,55);
   noStroke();
-  arc(width/2,height/2,width*9/16,height/2,0,PI);
+  arc(300,300,337.5,300,0,PI);
   //Right Wing
   fill(0,71,171);
   noStroke();
   arc(width/2,height/2,width*3/8,height*3/8,0,PI);
-  ellipseMode(RADIUS);
-  fill(255,147,44);
-  ellipse(width*5/64,height*13/64,width/16,height/16);
-  ellipseMode(CENTER);
-  fill(0,105,62);
-  ellipse(width*5/64,height*13/64,width/16,height/16);
-  ellipseMode(RADIUS);
-  fill(255,147,44);
-  ellipse(width*5/64,height*23/64,width/16,height/16);
-  ellipseMode(CENTER);
-  fill(0,105,62);
-  ellipse(width*5/64,height*23/64,width/16,height/16);
-  ellipseMode(RADIUS);
-  fill(255,147,44);
-  ellipse(width*7/32,height/8,width/16,height/16);
-  ellipseMode(CENTER);
-  fill(0,105,62);
-  ellipse(width*7/32,height/8,width/16,height/16);
-  ellipseMode(RADIUS);
-  fill(255,147,44);
-  ellipse(width*7/32,height*9/32,width/16,height/16);
-  ellipseMode(CENTER);
-  fill(0,105,62);
-  ellipse(width*7/32,height*9/32,width/16,height/16);
-  ellipseMode(RADIUS);
-  fill(255,147,44);
-  ellipse(width*7/32,height*7/16,width/16,height/16);
-  ellipseMode(CENTER);
-  fill(0,105,62);
-  ellipse(width*7/32,height*7/16,width/16,height/16);
-  ellipseMode(RADIUS);
-  fill(255,147,44);
-  ellipse(width*23/64,height*13/64,width/16,height/16);
-  ellipseMode(CENTER);
-  fill(0,105,62);
-  ellipse(width*23/64,height*13/64,width/16,height/16);
-  ellipseMode(RADIUS);
-  fill(255,147,44);
-  ellipse(width*23/64,height*23/64,width/16,height/16);
-  ellipseMode(CENTER);
-  fill(0,105,62);
-  ellipse(width*23/64,height*23/64,width/16,height/16);
-  ellipseMode(RADIUS);
-  fill(255,147,44);
-  ellipse(width/2,height*9/32,width/16,height/16);
-  ellipseMode(CENTER);
-  fill(0,105,62);
-  ellipse(width/2,height*9/32,width/16,height/16);
-  ellipseMode(RADIUS);
-  fill(255,147,44);
-  ellipse(width/2,height*7/16,width/16,height/16);
-  ellipseMode(CENTER);
-  fill(0,105,62);
-  ellipse(width/2,height*7/16,width/16,height/16);
+  eagleEye1.display();
+  eagleEye2.display();
+  eagleEye3.display();
+  eagleEye4.display();
+  eagleEye5.display();
+  eagleEye6.display();
+  eagleEye7.display();
+  eagleEye8.display();
+  eagleEye9.display();
 }
 function drawBeautifulMind() {
   //Comb
   fill(227,38,54);
   beginShape();
-  curveVertex(width*.775,height*.2);
-  curveVertex(width*.775,height*.2);
-  curveVertex(width*.775,height*.07);
-  curveVertex(width*.725,height*.07);
-  // curveVertex(width*.725,height*.2);
-  // curveVertex(width*.725,height*.2);
+  curveVertex(465,120);
+  curveVertex(465,120);
+  curveVertex(465,42);
+  curveVertex(435,42);
+  // curveVertex(435,120);
+  // curveVertex(435,120);
   // endShape();
   // beginShape();
-  curveVertex(width*.725,height*.2);
-  curveVertex(width*.725,height*.2);
-  curveVertex(width*.725,height*.05);
-  curveVertex(width*.675,height*.05);
-  // curveVertex(width*.675,height*.2);
-  // curveVertex(width*.675,height*.2);
+  curveVertex(435,120);
+  curveVertex(435,120);
+  curveVertex(435,30);
+  curveVertex(405,30);
+  // curveVertex(405,120);
+  // curveVertex(405,120);
   // endShape();
   // beginShape();
-  curveVertex(width*.675,height*.2);
-  curveVertex(width*.675,height*.2);
-  curveVertex(width*.675,height*.07);
-  curveVertex(width*.625,height*.07);
-  curveVertex(width*.625,height*.2);
-  curveVertex(width*.625,height*.2);
+  curveVertex(405,120);
+  curveVertex(405,120);
+  curveVertex(405,42);
+  curveVertex(375,42);
+  curveVertex(375,120);
+  curveVertex(375,120);
   endShape();
   //Head
   fill(0,106,78);
   beginShape();
-  curveVertex(width*25/32,height/2);
-  curveVertex(width*25/32,height/2);
-  curveVertex(width*25/32,height*0.15);
-  curveVertex(width*5/8,height*0.15);
-  curveVertex(width*5/8,height/2);
-  curveVertex(width*5/8,height/2);
+  curveVertex(468.75,300);
+  curveVertex(468.75,300);
+  curveVertex(468.75,90);
+  curveVertex(375,90);
+  curveVertex(375,300);
+  curveVertex(375,300);
   endShape();
   // //Waddle
   // fill(227,38,54);
   // beginShape();
-  // curveVertex(width*.78,height*.28);
-  // curveVertex(width*.78,height*.28);
-  // curveVertex(width*.78,height*.35);
-  // curveVertex(width*.82,height*.34);
-  // curveVertex(width*.78,height*.28);
-  // curveVertex(width*.78,height*.28);
+  // curveVertex(468,168);
+  // curveVertex(468,168);
+  // curveVertex(468,210);
+  // curveVertex(492,204);
+  // curveVertex(468,168);
+  // curveVertex(468,168);
   // endShape();
   // //Beak
   // fill(218,165,32);
-  // triangle(width*.78,height/5,width*.78,height*.3,width*.88,height/4);
+  // triangle(468,120,468,180,528,150);
   //Eyes
   fill(0);
-  circle(width*7/10,height*7/40,width/25);
-  circle(width*3/4,height*7/40,width/25);
+  circle(420,105,24);
+  circle(450,105,24);
 }
 function drawPedestal() {
   //Pedestal
   fill(72,6,7);
-  rect(width/8,height*3/4,width*3/4,height/20);
-  rect(width*7/32,height*0.8,width*9/16,height/5);
+  rect(75,450,450,30);
+  rect(131.25,480,337.5,120);
   fill(165,42,42);
-  square(width*.25,height*0.8,width*.2);
-  square(width*.55,height*0.8,width*.2);
+  square(150,480,120);
+  square(330,480,120);
 }
